55cdb8f184a47dd77b2f8ffe3f998c830a02a8fc
ElqBell <silenc8@gmail.com>
2020-10-05 22:03:12 +0300

    now showing motivational quotes

--- quotes/src/components/cured/cured.js
+++ quotes/src/components/cured/cured.js
@@ -1,16 +1,19 @@
 import React from "react"
+import quotes from "../../../static/quotes";
 
 export default function Cured() {
-    const quote = "epic quote PogU";
+    const quote = quotes[Math.floor((Math.random() * quotes.length))];
 
+    console.log("quote: ", quote);
     return (
         <article>
             <header>
                 <h1>You're fine now.</h1>
             </header>
             <section>
-                <h2>Before you leave, some motivating words for you:</h2>
-                <h2>{quote}</h2>
+                <h2>Before you leave, some words from fellow human beings for you:</h2>
+                <h2>{quote.quoteText}</h2>
+                <h2>-{quote.quoteAuthor}</h2>
             </section>
         </article>
     );
